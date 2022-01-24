37c344f159b2e281b0fd7a55c355aae7b1f8320b
woli95 <wolszczakd@gmail.com>
2022-01-25 00:10:35 +0100

    qweqwe

--- functions.py
+++ functions.py
@@ -1,8 +1,4 @@
 import shutil
 def getHardDiskUsage():
     total, used, free = shutil.disk_usage("/")
-    return {
-        total: total // (2**30),
-        used: used // (2**30),
-        free: free // (2**30)
-        }
+    return total // (2**30), used // (2**30), free // (2**30)
